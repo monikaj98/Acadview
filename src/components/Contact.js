d31e716520dc4269c9601ac5ae71591fb7d7fc3e
monikaj98 <monika1535.cse16@chitkara.edu.in>
2019-03-08 22:53:08 +0530

    react-routing-exercise

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Contact = (props) => {
+ // console.log(props);
+  // setTimeout(()=>{
+  //   props.history.push('/about')
+  // }, 2000)
+
+  return(
+    <div>
+      <div className='container center'>
+        <h4>Contact Page</h4>
+        <p> This my Contact page. </p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact;
